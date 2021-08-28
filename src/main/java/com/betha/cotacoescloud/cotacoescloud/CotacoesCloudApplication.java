a4367a95f965f6efd1db400bf49b3329f0079a97
henriquecechinel <henrique-cechinel@hotmail.com>
2021-08-28 08:39:57 -0300

    Inicialização do projeto

---
+++ src/main/java/com/betha/cotacoescloud/cotacoescloud/CotacoesCloudApplication.java
@@ -0,0 +1,13 @@
+package com.betha.cotacoescloud.cotacoescloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CotacoesCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CotacoesCloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/betha/cotacoescloud/cotacoescloud/CotacoesCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.betha.cotacoescloud.cotacoescloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CotacoesCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
