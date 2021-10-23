1649f90d32532872b85c6f1d95690fa89d3064af
Henrique Cechinel <henrique-cechinel@hotmail.com>
2021-10-23 11:51:08 -0300

    Cotinuando desenvolvimento

--- src/main/java/com/betha/cotacoescloud/cotacoescloud/model/Bairro.java
+++ src/main/java/com/betha/cotacoescloud/cotacoescloud/model/Bairro.java
@@ -2,17 +2,20 @@ package com.betha.cotacoescloud.cotacoescloud.model;
 
 import com.betha.cotacoescloud.cotacoescloud.enterprise.AbstractRegiao;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 public class Bairro extends AbstractRegiao {
     @ManyToOne
     @NotNull
     @JoinColumn(name = "I_MUNICIPIO", referencedColumnName = "ID")
-    private Municipio i_municipio;
+    private Municipio municipio;
 
     public Bairro(String nome, Long populacao, Municipio i_municipio) {
         super(nome, populacao);
