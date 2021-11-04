3a606b8690b701281789599bf540a07b52920e97
patrickpborges <patrick.borges@betha.com.br>
2021-11-04 15:17:34 -0300

    Incluído o findBy nas variáveis do ItemLicitacao e tirada uma importação desnecessária

--- src/main/java/com/betha/cotacoescloud/cotacoescloud/repository/ItemLicitacaoRepository.java
+++ src/main/java/com/betha/cotacoescloud/cotacoescloud/repository/ItemLicitacaoRepository.java
@@ -1,10 +1,23 @@
 package com.betha.cotacoescloud.cotacoescloud.repository;
 
 import com.betha.cotacoescloud.cotacoescloud.model.ItemLicitacao;
+import com.betha.cotacoescloud.cotacoescloud.model.Material;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ItemLicitacaoRepository extends JpaRepository<ItemLicitacao, Long>, QuerydslPredicateExecutor<ItemLicitacao> {
+
+    List<ItemLicitacao> findByNumeroItem(String numeroItem);
+
+    List<ItemLicitacao> findByQuantidade(String quantidade);
+
+    List<ItemLicitacao> findByValorUnitario(String valorUnitario);
+
+    List<ItemLicitacao> findByValorTotal(String valorTotal);
+
+
 }
