58f891ee56fb4d2c4e2dc6240f84062304717f36
patrickpborges <patrick.borges@betha.com.br>
2021-11-12 19:33:18 -0300

    Inserido os @GeneratedValue, @NotNull e @Column no Material

--- src/main/java/com/betha/cotacoescloud/cotacoescloud/model/Material.java
+++ src/main/java/com/betha/cotacoescloud/cotacoescloud/model/Material.java
@@ -2,11 +2,26 @@ package com.betha.cotacoescloud.cotacoescloud.model;
 
 import com.betha.cotacoescloud.cotacoescloud.enterprise.AbstractEntity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.validation.constraints.NotNull;
+
+@Entity
 public class Material extends AbstractEntity {
 
+    @GeneratedValue
+    @NotNull(message = "O campo Código é obrigatório!")
+    @Column(name = "CODIGO")
     private Long codigo;
+    @NotNull(message = "O campo Descrição é obrigatório!")
+    @Column(name = "DESCRICAO")
     private String descricao;
+    @NotNull(message = "O campo Unidade e Medida é obrigatório!")
+    @Column(name = "UNIDADE_MEDIDA")
     private UnidadeMedida unidadeMedida;
+    @NotNull(message = "O campo Tipo é obrigatório!")
+    @Column(name = "Tipo")
     private Tipo tipo;
 
     public Material(Long codigo, String descricao, UnidadeMedida unidadeMedida, Tipo tipo) {
