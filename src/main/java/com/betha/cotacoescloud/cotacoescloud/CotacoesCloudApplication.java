f9c45f7e75339db86621216e429c60adfb2ac217
patrickpborges <patrick.borges@betha.com.br>
2021-11-19 19:23:23 -0300

    Criado o CotacoesCloudApplication

--- src/main/java/com/betha/cotacoescloud/cotacoescloud/CotacoesCloudApplication.java
+++ src/main/java/com/betha/cotacoescloud/cotacoescloud/CotacoesCloudApplication.java
@@ -2,6 +2,10 @@ package com.betha.cotacoescloud.cotacoescloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class CotacoesCloudApplication {
@@ -10,4 +14,16 @@ public class CotacoesCloudApplication {
 		SpringApplication.run(CotacoesCloudApplication.class, args);
 	}
 
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedMethods("*").allowedOrigins("*");
+			}
+		};
+	}
+
 }
+
